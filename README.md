5f3bc19aacad629b6c50602dbbed56fc7e0619ec
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-07-09 21:28:51 -0700

    updates to javascript from july 9

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# javascript-timed-quiz
+# Timed Quiz on JavaScript
+
+# Description
+    -This project involved developing a quiz on the topic of JavaScript.
+    -The quiz includes 8 questions and allows for 180 seconds.
+    -If the user gets a question wrong, 15 seconds are removed from the timer.
+    -There is a high scores section where the user is able to determine how they did compared to others.
+    
+    
+# Installation
+
+    Follow the following URL to the website: 
+    
+# Usage
+<img src="" alt="screenshot of JavaScript Quiz" width="600px" />
+   
+    - Clicking the answer will bring the user to the next page.
+
+# Credits
+    Donnie Rawlings: https://github.com/drawlin22/
+    Received assistance from Tutor Neil Dhand.
+    
+
+
+# License
+    N/A
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,36 @@
-var timeLeft =document.querySelector(".time")
-var secondsLeft=180
+//constant
+var timeLeft =document.querySelector(".time");
+var replayEL = document.querySelector("#replay");
+var startEl =document.getElementById("start");
+var rulesHeader = document.getElementById("rulesHeader");
+var rules = document.getElementById("rules");
+var initials = document.getElementById("initials");
+var submit = document.getElementById("submit")
+var replay = document.getElementById("replay")
+
+// state variable
+var secondsLeft=120
+var currentQuestion = 0;
+
+replay.style.display = "none";
+initials.style.display = "none";
+submit.style.display = "none";
+
+startEl.addEventListener("click", ()=> {
+    startEl.style.display = "none";
+    rulesHeader.style.display = "none";
+    rules.style.display = "none";
+    
+    
+    startQuiz ()
+});
+
+
 var questions = [
     {
         question: "What does boolean include?",
         choices: ["true", "false" ,"more than one of the above", "none of the above"],
-        answer: "2"
+        answer: "more than one of the above"
     },
     {  
         question: "Which is not a most common variable type?",
@@ -19,7 +45,7 @@ var questions = [
     //Question help from https://www.sanfoundry.com/1000-javascript-questions-answers/
     {
         question: "Arrays in JavaScript are defined by which of the following?",
-        choices: ["an ordered list of values", " an ordered list of    objects", "an ordered list of strings", "an ordered list of functions"],
+        choices: ["an ordered list of values", " an ordered list of objects", "an ordered list of strings", "an ordered list of functions"],
         answer: "an ordered list of values"
     },
     {
@@ -29,7 +55,7 @@ var questions = [
     },
     {
         question: "Which of the following is not an error in JavaScript?",
-        choices: ["missing a bracket", "missing a semicolon", "dividing by zero", "mismatched capitalization" ],
+        choices: ["missing a bracket", "missing a semicolon", "dividing by zero", "mismatched capitalization"],
         answer: "dividing by zero"
     },
     //help with question from https://www.javatpoint.com/javascript-mcq
@@ -44,37 +70,42 @@ var questions = [
         answer: "everything in the object"
     },
 ];
-var currentQuestion = 0;
+
 
 function setTime () {
     var timerInterval= setInterval(function(){
         secondsLeft--;
-        timeLeft.textContent =secondsLeft + "seconds left";
+        timeLeft.textContent =secondsLeft + " seconds left";
 //add in if the questions were incorrect take time off
-    if (secondsLeft ===0) {
-        alert ("time out")
-        clearInterval()
+    if (secondsLeft <=0) {
+        alert ("Out of Time!");
+        clearInterval(timerInterval);
+        endQuiz()
     }
 }, 1000)
 }
 
 function displayQuestion(question, choices) {
-    // TODO: clear existing text
-    const questionText = document.createTextNode(question)
-    document.getElementById('question').appendChild(questionText)
+   
     
+    const questionText = document.createTextNode(question)
+    
+    document.getElementById('question').appendChild(questionText)
     for(let i = 0; i < choices.length; i++) {
-        const li = document.createElement('li');
+       
+        const li = document.createElement('button');
         const textNode = document.createTextNode(choices[i])
         li.appendChild(textNode);
         li.addEventListener("click", answerQuestion);
         document.getElementById("answerChoices").appendChild(li)
+       
     }
 }
 
 function startQuiz() {
     displayQuestion(questions[currentQuestion].question, questions[currentQuestion].choices);
     setTime()
+
 }
 //displayQuestion('Which is not a most common variable type?', [string, number,boolean, textNode])
 
@@ -84,15 +115,46 @@ function answerQuestion(event) {
     console.log(event)
     if(userChoice === questions[currentQuestion].answer) {
         console.log('correct!')
+        secondsLeft+=5 
+        document.getElementById('question').innerHTML = "";
+    document.getElementById('answerChoices').innerHTML = "";
     } else {
         console.log('incorrect!')
         // decrease time
+        secondsLeft-=15
+        document.getElementById('question').innerHTML = "";
+    document.getElementById('answerChoices').innerHTML = "";
     }
+  
     // increment currentQuestion
     currentQuestion++
     // call displayQuestion again
     displayQuestion(questions[currentQuestion].question, questions[currentQuestion].choices);
+     
+    
+    //this does not work
+    if (currentQuestion === undefined || secondsLeft <= 0){
+        endQuiz();
+        window.alert("Quiz Over! Please put your Initials in the box");
+    }
 
 }
+function endQuiz() {
+    //need to add if out of questions or timer <=0 then alert quiz is over
+    document.getElementById('question').innerHTML = "";
+    document.getElementById('answerChoices').innerHTML = ""
+    if (currentQuestion === undefined || secondsLeft <= 0) {
+        window.alert("Quiz Over! Please put your Initials in the box");
+        clearInterval(setTime);
+    }
+    // stop the timer if out of questions
+    //need to save the time that ended (secondsLeft)
+    //need to add replay button-this doesnt work needs to clear the current
 
-startQuiz()
+    replayEL.addEventListener("click", startQuiz)
+    //need to add saving system of high scores (initials plus secondsLeft)
+    initials.style.display = "block";
+    submit.style.display = "block";
+    replay.style.display = "block";
+}
+
